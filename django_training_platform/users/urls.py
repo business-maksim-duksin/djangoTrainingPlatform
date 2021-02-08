6e1176531663b91d54b246b0be7272c4a5ef4b1d
maksimduksin <business.maxtheduksin@gmail.com>
2021-02-08 16:44:48 +0300

    Addwd UserList view. Now users can search users. Made for possibility to search for an user's id when creating a membership.

--- django_training_platform/users/urls.py
+++ django_training_platform/users/urls.py
@@ -2,8 +2,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserCreateView
+from .views import UserCreateView, UserListView
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 app_name = 'users'
 
@@ -11,5 +12,11 @@ urlpatterns = [
     path('register', UserCreateView.as_view()),
     path('token', TokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
+    # path('users', UserListView.as_view()),
 ]
 
+router = DefaultRouter()
+router.register(r"users", UserListView)
+urlpatterns += [
+    path('', include(router.urls)),
+]
--- django_training_platform/users/views.py
+++ django_training_platform/users/views.py
@@ -1,10 +1,24 @@
 from rest_framework import generics, permissions
+from rest_framework import permissions, viewsets, mixins
 from . import serializers as s
+from core.permissions import IsTeacher
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import get_user_model
 
+# from django.conf import settings
+# User = settings.AUTH_USER_MODEL
 
 
 # Create your views here.
 class UserCreateView(generics.CreateAPIView):
     serializer_class = s.UserSerializer
     permission_classes = (permissions.AllowAny,)
+
+
+class UserListView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = s.UserSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = (permissions.IsAuthenticated, IsTeacher)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username', 'first_name', 'last_name', 'is_teacher']
 
