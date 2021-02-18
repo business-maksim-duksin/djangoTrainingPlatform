4fbc719648ef9e22795173b7ccbb9e707cde7f09
maksimduksin <business.maxtheduksin@gmail.com>
2021-02-18 13:01:46 +0300

    WIP populate db func

--- django_training_platform/utils/factories.py
+++ django_training_platform/utils/factories.py
@@ -8,12 +8,32 @@ from training_platform import models as m
 
 
 class UserFactory(DjangoModelFactory):
+    username = Faker("user_name")
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
+    email = Faker("email")
 
-    # username = Faker("user_name")
-    # email = Faker("email")
     # name = Faker("name")
     # password = LazyAttribute(lambda o: o.username)
-    password = SelfAttribute("username")
+
+    # password = SelfAttribute("username")
+
+    @post_generation
+    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
+        password = (
+            extracted
+            if extracted
+            else SelfAttribute("username")
+            # else Faker(
+            #     "password",
+            #     length=42,
+            #     special_chars=True,
+            #     digits=True,
+            #     upper_case=True,
+            #     lower_case=True,
+            # ).evaluate(None, None, extra={"locale": None})
+        )
+        self.set_password(password)
 
     class Meta:
         model = get_user_model()
@@ -21,28 +41,56 @@ class UserFactory(DjangoModelFactory):
 
 
 class CourseFactory(DjangoModelFactory):
+    name = Faker("word")
+    description = Faker("paragraph")
 
     class Meta:
         model = m.Course
-        django_get_or_create = ["id"]
+        django_get_or_create = ["name"]
 
 
 class MembershipFactory(DjangoModelFactory):
-
     class Meta:
         model = m.Membership
         django_get_or_create = ["user", "course"]
 
 
 class LessonFactory(DjangoModelFactory):
+    name = Faker("word")
+    content = Faker("paragraph")
 
     class Meta:
         model = m.Lesson
-        django_get_or_create = ["id"]
+        django_get_or_create = ["name", "course"]
 
 
 class TaskFactory(DjangoModelFactory):
+    content = Faker("paragraph")
 
     class Meta:
-        model = m.Lesson
-        django_get_or_create = ["id"]
+        model = m.Task
+        django_get_or_create = ["lesson", "course"]
+
+
+class CompletedTaskFactory(DjangoModelFactory):
+    content = Faker("paragraph")
+
+    class Meta:
+        model = m.CompletedTask
+        django_get_or_create = ["task", "course"]
+
+
+class GradeFactory(DjangoModelFactory):
+    amount = Faker("random_int", min=0, max=100)
+
+    class Meta:
+        model = m.Grade
+        django_get_or_create = ["completed_task", "course"]
+
+
+class CommentFactory(DjangoModelFactory):
+    content = Faker("sentence")
+
+    class Meta:
+        model = m.Comment
+        django_get_or_create = ["grade", "course"]
--- django_training_platform/utils/sample_populate_db.py
+++ django_training_platform/utils/sample_populate_db.py
@@ -1,9 +1,63 @@
 from training_platform.models import *
 from django.contrib.auth import get_user_model
-from factories import UserFactory
+from . import factories as f
 
 
 
 def sample_populate_db():
-    user = UserFactory(username="s1", is_teacher=False)
-    pass
+    student_1 = f.UserFactory(username='s1', is_teacher=False)
+    student_2 = f.UserFactory(username='s2', is_teacher=False)
+    student_3 = f.UserFactory(username='s3', is_teacher=False)
+
+    teacher_math = f.UserFactory(username='t_math', is_teacher=True)
+    teacher_stat = f.UserFactory(username='t_stat', is_teacher=True)
+
+# MATH COURSE
+# ------------------------------------------------------------------------------
+    course_math = f.CourseFactory(name='math', owner=teacher_math)
+
+    f.MembershipFactory(owner=teacher_math, course=course_math, user=teacher_math)
+    f.MembershipFactory(owner=teacher_math, course=course_math, user=teacher_stat)
+    f.MembershipFactory(owner=teacher_math, course=course_math, user=student_1)
+    f.MembershipFactory(owner=teacher_math, course=course_math, user=student_2)
+    f.MembershipFactory(owner=teacher_math, course=course_math, user=student_3)
+
+    l1 = f.LessonFactory(owner=teacher_math, course=course_math, name="math_1")
+    l2 = f.LessonFactory(owner=teacher_math, course=course_math, name="math_2")
+
+    t1 = f.TaskFactory(owner=teacher_math, course=course_math, lesson=l1,)
+    t2 = f.TaskFactory(owner=teacher_math, course=course_math, lesson=l1,)
+    t3 = f.TaskFactory(owner=teacher_stat, course=course_math, lesson=l2, )
+
+    ct1_1 = f.CompletedTaskFactory(owner=student_1, course=course_math, task=t1)
+    gr1 = f.GradeFactory(owner=teacher_math, course=course_math, completed_task=ct1_1)
+    com1 = f.CommentFactory(owner=teacher_math, course=course_math, grade=gr1)
+    com2 = f.CommentFactory(owner=student_1, course=course_math, grade=gr1)
+    com3 = f.CommentFactory(owner=teacher_math, course=course_math, grade=gr1)
+    gr2 = f.GradeFactory(owner=teacher_math, course=course_math, completed_task=ct1_1)
+
+    ct1_2 = f.CompletedTaskFactory(owner=student_1, course=course_math, task=t1)
+
+    ct1_3 = f.CompletedTaskFactory(owner=student_2, course=course_math, task=t1)
+    gr2 = f.GradeFactory(owner=teacher_math, course=course_math, completed_task=ct1_3)
+
+    ct1_4 = f.CompletedTaskFactory(owner=student_3, course=course_math, task=t1)
+    gr3 = f.GradeFactory(owner=teacher_math, course=course_math, completed_task=ct1_4)
+
+    ct2_1 = f.CompletedTaskFactory(owner=student_1, course=course_math, task=t2)
+    ct2_2 = f.CompletedTaskFactory(owner=student_2, course=course_math, task=t2)
+    ct2_3 = f.CompletedTaskFactory(owner=student_3, course=course_math, task=t2)
+    ct3_1 = f.CompletedTaskFactory(owner=student_2, course=course_math, task=t3)
+    gr4 = f.GradeFactory(owner=teacher_math, course=course_math, completed_task=ct3_1)
+    ct3_2 = f.CompletedTaskFactory(owner=student_3, course=course_math, task=t3)
+    gr5 = f.GradeFactory(owner=teacher_stat, course=course_math, completed_task=ct3_2)
+
+
+
+
+
+
+
+
+
+
